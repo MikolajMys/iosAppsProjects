bbd32adb76b38cecc1e24716831d8215c35d806f
Mikołaj <mikolaj.my@icloud.com>
2024-08-29 01:01:31 +0200

    first look

--- MyApp/MyApp/ContentView.swift
+++ MyApp/MyApp/ContentView.swift
@@ -10,12 +10,84 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Solve this cube!")
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(color: .black, radius: 4, x: 5, y: 5)
+                .padding(.bottom, 50)
+            HStack {
+                VStack {
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("1")
+                                .foregroundColor(.white)
+                        )
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("4")
+                                .foregroundColor(.white)
+                        )
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("7")
+                                .foregroundColor(.white)
+                        )
+                }
+                VStack {
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("2")
+                                .foregroundColor(.white)
+                        )
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("5")
+                                .foregroundColor(.white)
+                        )
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("8")
+                                .foregroundColor(.white)
+                        )
+                    
+                }
+                VStack {
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("3")
+                                .foregroundColor(.white)
+                        )
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                        .overlay(
+                            Text("6")
+                                .foregroundColor(.white)
+                        )
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 30, height: 30)
+                }
+            }
+            .padding(.all, 10)
+            .background(Color.black.opacity(0.6))
+        .shadow(color: .black, radius: 10, x: 5, y: 5)
         }
-        .padding()
     }
 }
 
