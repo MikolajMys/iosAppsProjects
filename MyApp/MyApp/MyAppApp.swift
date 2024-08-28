ee54a2c7d46b4429a42e253933afb8003ea07c11
Mikołaj <mikolaj.my@icloud.com>
2024-08-29 00:30:23 +0200

    initial commit

---
+++ MyApp/MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Mikołaj Myśliński on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
