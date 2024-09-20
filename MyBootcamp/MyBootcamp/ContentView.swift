6ab204639ec3ff2a1fbd0b74329f6032de744408
Mikołaj <mikolaj.my@icloud.com>
2024-09-20 11:28:31 +0200

    Preview of MyBootcamp project

--- MyBootcamp/MyBootcamp/ContentView.swift
+++ MyBootcamp/MyBootcamp/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    let shadowColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    let shadowBGColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     var body: some View {
-        Text("hell")
-            
+        ZStack {
+            LinearGradient(
+                colors: [.blue, .purple, .pink, .orange, .yellow],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.white) // Kolor obiektu
+                .frame(width: 300, height: 150)
+                .overlay(
+                    Text("MyBootcamp")
+                        .font(.system(size: 40, weight: .bold, design: .default))
+                        .foregroundColor(.white) // Tekst musi być przezroczysty
+                        .blendMode(.destinationOut) // Efekt "wycięcia"
+                )
+                .compositingGroup()
+        }
     }
 }
 
