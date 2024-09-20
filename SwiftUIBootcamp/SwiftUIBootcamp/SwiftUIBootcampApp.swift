81f38f5dadf9d1cc497f0d482400d15d068b2c62
Mikołaj <mikolaj.my@icloud.com>
2024-09-20 10:10:22 +0200

    initial commit for new project

---
+++ SwiftUIBootcamp/SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Mikołaj Myśliński on 20/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
