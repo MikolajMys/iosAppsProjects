04b1b96b5c6ab031487f10501c09e14625e0e8fd
Mikołaj <mikolaj.my@icloud.com>
2024-09-20 12:25:34 +0200

    Navigation View Bootcamp

--- MyBootcamp/MyBootcamp/NavigationViewBootcamp.swift
+++ MyBootcamp/MyBootcamp/NavigationViewBootcamp.swift
@@ -9,7 +9,71 @@ import SwiftUI
 
 struct NavigationViewBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                
+                NavigationLink("Hello, world!") {
+                    MyOtherScreen()
+                }
+                
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+            .navigationTitle("All Inboxes")
+            //.navigationBarTitleDisplayMode(.automatic)
+            //.navigationBarHidden(true) // old way of making navbar dissapear
+            //.toolbar(.hidden, for: .navigationBar)
+//            .navigationBarItems( // old code
+//                leading: Image(systemName: "person.fill"),
+//                trailing: Image(systemName: "gear")
+//            )
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {
+                        Image(systemName: "person.fill")
+                        Image(systemName: "flame.fill")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: MyOtherScreen()) {
+                        Image(systemName: "gear")
+                            //.foregroundColor(.red) // alternative color icon change
+                    }
+                    //.accentColor(Color.red) //old code
+                    .tint(.red)
+                }
+            }
+        }
+    }
+}
+
+struct MyOtherScreen: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            Color.green
+                .ignoresSafeArea()
+                .navigationTitle("Green Screen!")
+                //.toolbar(.hidden, for: .navigationBar)
+            
+            VStack {
+                HStack {
+                    Button("< BACK BUTTON") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .padding()
+                    Spacer()
+                }
+                Spacer()
+                NavigationLink("Click here") {
+                    Text("3rd screen!")
+                }
+                Spacer()
+            }
+        }
     }
 }
 
