d680d63030c43b013c05f783f8e1341cae74e001
Mikołaj <mikolaj.my@icloud.com>
2024-10-20 05:20:37 +0200

    Ai Photo Face Detector - nice version

--- FaceDetectionApp/FaceDetectionApp/ContentView.swift
+++ FaceDetectionApp/FaceDetectionApp/ContentView.swift
@@ -9,16 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var image: UIImage = UIImage(named: "photo6")!
-//    @State private var images: [UIImage] = [
-//        UIImage(named: "photo0")!,
-//        UIImage(named: "photo1")!,
-//        UIImage(named: "photo2")!,
-//        UIImage(named: "photo3")!,
-//        UIImage(named: "photo4")!,
-//        UIImage(named: "photo5")!,
-//        UIImage(named: "photo6")!
-//    ]
+    @State private var currentIndex: Int = 0
+    @State private var image: UIImage = UIImage(named: "photo0")!
+    @State private var images: [UIImage] = [
+        UIImage(named: "photo0")!,
+        UIImage(named: "photo1")!,
+        UIImage(named: "photo2")!,
+        UIImage(named: "photo3")!,
+        UIImage(named: "photo4")!,
+        UIImage(named: "photo5")!,
+        UIImage(named: "photo6")!
+    ]
+    @State private var detectedFaces: [Int] = [0,0,0,0,0,0,0]
+    
+    @State private var detectedImages: [UIImage: UIImage] = [:]
+    @State private var isDetectingImage = false
+    @State private var refresh = UUID()
+    
     @ObservedObject var faceDetector = DetectFaces()
     
     let bgColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
@@ -27,42 +34,89 @@ struct ContentView: View {
         ZStack {
             Color(bgColor).opacity(0.5).ignoresSafeArea()
             VStack {
-                ZStack {
-                    if faceDetector.outputImage != nil {
-                        Image(uiImage: faceDetector.outputImage!)
-                            .resizable()
-                            .scaledToFit()
+                VStack(alignment: .center) {
+                    if isDetectingImage {
+                        ProgressView("Detecting...")
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            //.background(Color(bgColor).opacity(0.5))
+                            .cornerRadius(30)
                     } else {
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFit()
+                        TabView(selection: $currentIndex) {
+                            ForEach(images.indices, id: \.self) { index in
+    //                            Image(uiImage: image)
+    //                                .resizable()
+    //                                .scaledToFit()
+    //                                .tag(image)
+                                if let detectedImage = detectedImages[images[index]] {
+                                    Image(uiImage: detectedImage)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .tag(index)
+                                } else {
+                                    Image(uiImage: images[index])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .tag(index)
+                                    
+                                }
+                            }
+                            .cornerRadius(30)
+                        }
+                        .tabViewStyle(PageTabViewStyle())
+                        .disabled(isDetectingImage)
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Button {
+                            isDetectingImage = true
+                            faceDetector.inputImage = images[currentIndex]
+                            faceDetector.detectFaces(in: images[currentIndex])
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                if let output = faceDetector.outputImage {
+                                    detectedImages[images[currentIndex]] = output
+                                }
+                                detectedFaces[currentIndex] = faceDetector.detectedFacesCount
+                                isDetectingImage = false
+                                refresh = UUID()
+                            }
+                        } label: {
+                            Image(systemName: "face.smiling")
+                                .font(.largeTitle)
+                                .tint(Color.white)
+                        }
+                        .disabled(isDetectingImage)
+                        Spacer()
+                        Text("Detected Faces: \(detectedFaces[currentIndex])")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .disabled(isDetectingImage)
+                        Spacer()
+                        Button {
+                            isDetectingImage = true
+                            //faceDetector.outputImage = nil
+                            detectedImages[images[currentIndex]] = nil
+                            detectedFaces[currentIndex] = 0
+                            isDetectingImage = false
+                            refresh = UUID()
+                        } label: {
+                            Image(systemName: "gobackward")
+                                .font(.largeTitle)
+                                .tint(Color.white)
+                        }
+                        .disabled(isDetectingImage)
                     }
                 }
-                .cornerRadius(30)
                 .padding(20)
                 .shadow(color: .black, radius: 10, x: 5, y: 5)
-                //.padding(.bottom, 50)
+                .frame(height: 500)
+                //.padding(.bottom, 20)
                 .background(Color.black.opacity(0.5))
                 .cornerRadius(30)
-                HStack{
-                    Button {
-                        faceDetector.inputImage = image
-                        faceDetector.detectFaces(in: image)
-                    } label: {
-                        Text("Process Image")
-                    }
-                    Spacer()
-                    Button {
-                        faceDetector.outputImage = nil
-                    } label: {
-                        Text("Reset Image")
-                    }
-                }
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.black.opacity(0.5))
-                .cornerRadius(20)
+                .id(refresh)
             }
         }
 //        if faceDetector.outputImage != nil {
