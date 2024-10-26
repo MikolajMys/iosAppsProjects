03a641f390777f89ea680c0626772e060d8e1ddd
Mikołaj <mikolaj.my@icloud.com>
2024-10-26 11:10:39 +0200

    Body Pose Detection - Initial Commit

---
+++ BodyPoseDetectionApp/BodyPoseDetectionApp/BodyPoseDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyPoseDetectionAppApp.swift
+//  BodyPoseDetectionApp
+//
+//  Created by Mikołaj Myśliński on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BodyPoseDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
