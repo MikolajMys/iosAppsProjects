802551992e616fe5f542e2bc60ed03163310a9f4
Mikołaj <mikolaj.my@icloud.com>
2024-10-27 02:34:24 +0200

    Camera Stream setup

--- BodyPoseDetectionApp/BodyPoseDetectionApp/ContentView.swift
+++ BodyPoseDetectionApp/BodyPoseDetectionApp/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var model = FrameHandler()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        FrameView(image: model.frame)
+            //.ignoresSafeArea()
     }
 }
 
