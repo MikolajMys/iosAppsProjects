91eee30c020815dae238b1c34f61d388c970327a
Mikołaj <mikolaj.my@icloud.com>
2024-12-11 19:25:43 +0100

    On Appear and On Disappear Bootcamp

---
+++ MyBootcamp/MyBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,51 @@
+//
+//  OnAppearBootcamp.swift
+//  MyBootcamp
+//
+//  Created by Mikołaj Myśliński on 11/12/2024.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    
+    @State var myText: String = "Start text."
+    @State var count: Int = 0
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Text(myText)
+                LazyVStack {
+                    ForEach(0..<50) { _ in
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear { // This is the same as with the word perform
+                                //print("Rectangle appeared")
+                                count += 1
+                            }
+                            .onDisappear {
+                                count -= 1
+                            }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    myText = "This is the new text!"
+                }
+            })
+            .onDisappear(perform: {
+                myText = "Ending text."
+            })
+            .navigationTitle("On Appear: \(count)")
+        }
+    }
+}
+
+struct OnAppearBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        OnAppearBootcamp()
+    }
+}
