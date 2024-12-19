2fc29150474eae475293b04a81061602799fa6b6
Mikołaj <mikolaj.my@icloud.com>
2024-12-19 12:35:23 +0100

    @ObservableObject and @StateObject Bootcamp

---
+++ MyBootcamp/MyBootcamp/ViewModelBootcamp.swift
@@ -0,0 +1,122 @@
+//
+//  ViewModelBootcamp.swift
+//  MyBootcamp
+//
+//  Created by Mikołaj Myśliński on 18/12/2024.
+//
+
+import SwiftUI
+
+struct FruitModel: Identifiable {
+    let id: String = UUID().uuidString
+    let name: String
+    let count: Int
+}
+
+class FruitViewModel: ObservableObject {
+    
+    @Published var fruitArray: [FruitModel] = [] // @Published is the same as @State but in class
+    @Published var isLoading: Bool = false
+    
+    init() {
+        getFruits()
+    }
+    
+    func getFruits() {
+        let fruit1 = FruitModel(name: "Oranges", count: 16)
+        let fruit2 = FruitModel(name: "Bananas", count: 2)
+        let fruit3 = FruitModel(name: "Watermelons", count: 88)
+        
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.fruitArray.append(fruit1)
+            self.fruitArray.append(fruit2)
+            self.fruitArray.append(fruit3)
+            self.isLoading = false
+        }
+    }
+    
+    func deleteFruit(index: IndexSet) {
+        fruitArray.remove(atOffsets: index)
+    }
+    
+}
+
+struct ViewModelBootcamp: View {
+    
+//    @State var fruitArray: [FruitModel] = []
+    // @StateObject -> USE THIS ON CREATION / INIT
+    // @ObservedObject -> USE THIS FOR SUBVIEW
+    @StateObject var fruitViewModel: FruitViewModel = FruitViewModel() // @StateObject is not refreshable
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                if fruitViewModel.isLoading {
+                    ProgressView()
+                } else {
+                    ForEach(fruitViewModel.fruitArray) { fruit in
+                        HStack {
+                            Text("\(fruit.count)")
+                                .foregroundColor(.red)
+                            Text(fruit.name)
+                                .font(.headline)
+                                .bold()
+                        }
+                    }
+                    .onDelete(perform: fruitViewModel.deleteFruit)
+                }
+            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Fruit List")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: RandomScreen(fruitViewModel: fruitViewModel)) {
+                        Image(systemName: "arrow.right")
+                            .font(.title)
+                    }
+                }
+            }
+//            .onAppear {
+//                fruitViewModel.getFruits()
+//            }
+        }
+    }
+}
+
+struct RandomScreen: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @ObservedObject var fruitViewModel: FruitViewModel
+    
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+//            Button {
+//                dismiss()
+//            } label: {
+//                Text("GO BACK")
+//                    .foregroundColor(.white)
+//                    .font(.largeTitle)
+//                    .fontWeight(.semibold)
+//            }
+            
+            VStack {
+                ForEach(fruitViewModel.fruitArray) { fruit in
+                    Text(fruit.name)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
+            }
+
+        }
+    }
+}
+
+struct ViewModelBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModelBootcamp()
+    }
+}
