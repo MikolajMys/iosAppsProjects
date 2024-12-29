adcb0daed97ca581c205eded02cee38ea7bae292
Mikołaj <mikolaj.my@icloud.com>
2024-12-29 17:43:39 +0100

    update

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -12,10 +12,12 @@ struct ContentView: View {
     @StateObject private var locationManager = LocationManager()
     @State private var weatherData: WeatherData?
     @State private var cityName: String = ""
+    @State private var alertMessage: String = ""
     @State private var citySearching: Bool = false
+    @State private var showAlert = false
     
     
-    let detailColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+    //let detailColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
     
     var body: some View {
         ZStack {
@@ -38,15 +40,22 @@ struct ContentView: View {
                         Button {
                             if cityName.isEmpty {
                                 locationManager.requestLocation()
+                            } else {
+                                alertMessage = "Unable to get location while using city name search!"
+                                showAlert = true
                             }
                         } label: {
                             Image(systemName: "location.circle")
-                                
                         }
                     }
                     .padding(.horizontal, 20)
                     .frame(width: 300, height: 50)
                     .background(Color("DetailColor").opacity(0.5))
+                    .alert("Unable to Proceed", isPresented: $showAlert) {
+                        Button("OK", role: .cancel) {}
+                    } message: {
+                        Text(alertMessage)
+                    }
                     Spacer()
                     Text(String(format: "%.1f°C", weatherData.main.temp))
                         .font(.custom("", size: 70))
@@ -93,7 +102,7 @@ struct ContentView: View {
     }
     
     private func fetchWeatherData(for cityName: String) {
-        let apiKey = "3e0e86760eada5a4076df6ba50dff736"
+        let apiKey = "YOUR_API_KEY"
         let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&units=metric&appid=\(apiKey)"
         guard let url = URL(string: urlString) else {return}
         
@@ -115,7 +124,7 @@ struct ContentView: View {
     }
     
     private func fetchWeatherData(for location: CLLocation) {
-        let apiKey = "3e0e86760eada5a4076df6ba50dff736"
+        let apiKey = "YOUR_API_KEY"
         let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&units=metric&appid=\(apiKey)"
         
         guard let url = URL(string: urlString) else {return}
