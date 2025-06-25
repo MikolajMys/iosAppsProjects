fae92c4a04aee236f72f1bef50a16309c19255c9
Mikołaj <mikolaj.my@icloud.com>
2025-06-25 08:00:12 +0200

    Number drawing detection app initial commit

---
+++ NumberDrawingDetectionApp/NumberDrawingDetectionApp/NumberDrawingDetectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberDrawingDetectionAppApp.swift
+//  NumberDrawingDetectionApp
+//
+//  Created by Mikołaj Myśliński on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct NumberDrawingDetectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
